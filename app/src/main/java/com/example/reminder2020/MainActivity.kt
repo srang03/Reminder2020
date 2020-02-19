a05af6b2cfa1cdb99d8957c1c4cad12c2c4a8eb1
srang03 <srang9503@gmail.com>
2020-02-19 12:37:44 +0200

    HW3

--- app/src/main/java/com/example/reminder2020/MainActivity.kt
+++ app/src/main/java/com/example/reminder2020/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.reminder2020
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
@@ -73,12 +78,33 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
     companion object {
-//
-//        fun showNotification(context: Context, message: String){
-//            val CHANNEL_ID = "REMINDER_NOTIFICATION_CHANNEL"
-//            var notificationBuilder = NotificationCompat.Builder(context, CHANNEL_ID)
-//                .setSamll
+
+        fun showNotification(context: Context, message: String){
+            val NotificationID = 1567
+            val CHANNEL_ID = "REMINDER_NOTIFICATION_CHANNEL"
+            var notificationBuilder = NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_alarm)
+                .setContentTitle(context?.getString(R.string.app_name))
+                .setContentText(message)
+                .setStyle( NotificationCompat.BigTextStyle().bigText(message))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            var notificationManager=context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.Q){
+                val channel = NotificationChannel(
+                    CHANNEL_ID,
+                    context.getString(R.string.app_name),
+                    NotificationManager.IMPORTANCE_DEFAULT
+                ).apply { description = context.getString(R.string.app_name) }
+
+                notificationManager.createNotificationChannel(channel)
+            }
+
+            }
+
+
         }
     }
-}
+
--- app/src/main/java/com/example/reminder2020/ReminderReceiver.kt
+++ app/src/main/java/com/example/reminder2020/ReminderReceiver.kt
@@ -3,14 +3,16 @@ package com.example.reminder2020
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import org.jetbrains.anko.toast
+import com.google.android.gms.location.GeofencingEvent
 
 
 class ReminderReceiver : BroadcastReceiver() {
-    override fun onReceive(context: Context, intent: Intent) {
+    override fun onReceive(context: Context, intent: Intent?) {
+        val geofencingEvent = GeofencingEvent.fromIntent(intent)
+//        val geofencingTransition= geofencingEvent.geo
 
-        val text = intent.getStringExtra("message")
-        context.toast(text)
+//        val text = intent.getStringExtra("message")
+//        context.toast(text)
     }
 
 }
